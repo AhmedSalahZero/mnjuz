e054afd9ddcc23eec98a9387de76f764c02d8cbb
Ahmed Salah <asalahdev5@gmail.com>
2026-01-14 00:53:41 +0200

    add modes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
             git status
             git stash
             git pull origin master
-            /usr/local/bin/ea-php84 artisan optimize:clear
+            /usr/bin/php8.4 artisan optimize:clear
             chmod -R 775 storage
             chmod -R 775 bootstrap/cache
             chmod 777 -R storage/*
-            /usr/local/bin/ea-php84 artisan migrate --force
+            /usr/bin/ea-php8.4 artisan migrate --force
     
             supervisord -c /etc/supervisord.conf
             supervisorctl restart all
