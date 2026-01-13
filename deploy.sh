b061a0e877ba12e92744b526560f9932fe421e90
Ahmed Salah <asalahdev5@gmail.com>
2026-01-14 01:13:33 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
             git status
             git stash
             git pull origin master
-            /usr/bin/php8.4 artisan optimize:clear
+            /usr/bin/php8.3 artisan optimize:clear
             chmod -R 775 storage
             chmod -R 775 bootstrap/cache
             chmod 777 -R storage/*
-            /usr/bin/php8.4 artisan migrate --force
+            /usr/bin/php8.3 artisan migrate --force
     
             supervisord -c /etc/supervisord.conf
             supervisorctl restart all
