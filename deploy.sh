07806b0b6378107a4dab8f53308d0c733be2b5e6
Ahmed Salah <asalahdev5@gmail.com>
2026-02-07 02:25:26 +0200

    e

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
             git status
             git stash
             git pull origin master
-            /usr/bin/php8.3 artisan optimize:clear
             chmod -R 775 storage
             chmod -R 775 bootstrap/cache
             chmod 777 -R storage/*
 	    $(which php) artisan migrate --force
-	   sudo chmod 777 ./deploy
 	  $(which php) artisan optimize:clear 
             sudo supervisorctl restart all
