1af9f9f8cec4f79be78db4c4d79d8cb7e311b65d
Ahmed Salah <asalahdev5@gmail.com>
2026-02-14 14:59:12 +0200

    test1

--- app/Events/NewChatEvent.php
+++ app/Events/NewChatEvent.php
@@ -160,7 +160,7 @@ class NewChatEvent implements ShouldBroadcast
 
     public function minimalChatValue($value): array
     {
-		return $value;
+		return (array) $value;
         $arr = $value instanceof \Illuminate\Database\Eloquent\Model
             ? $value->toArray()
             : (array) $value;
