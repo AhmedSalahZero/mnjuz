283a203f4e76224375656fb42d36406ad428f74f
Ahmed Salah <asalahdev5@gmail.com>
2026-08-21 17:32:51 +0300

    after add reverb support

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
             chmod -R 775 storage
             chmod -R 775 bootstrap/cache
             chmod 777 -R storage/*
-	        php8.3 artisan migrate --force
-	        php8.3 artisan optimize:clear 
+	    $(which php) artisan migrate --force
+	    $(which php) artisan optimize:clear 
             sudo supervisorctl restart all
